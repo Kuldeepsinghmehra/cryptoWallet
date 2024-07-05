3ef04eaa212eb76fd7bf1656a66e8c1f078062c9
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-07-05 22:55:07 +0530

    added font

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Sidenav = () => {
+  return <Box 
+  display="flex"
+  bg="green"
+  
+  >Sidenav</Box>
+ 
+   
+}
+
+export default Sidenav
